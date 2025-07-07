6c0d3e2ca8865c7bbe0fa803b6bebe0b34b9abc3
SmallestMeow <30783612+SmallestMeow@users.noreply.github.com>
2025-07-07 16:28:45 -0700

    Pruning folders

--- home/hack.js
+++
@@ -1,7 +0,0 @@
-// servers/home/hack.js
-async function main(ns) {
-  await ns.hack(ns.args[0], { additionalMsec: ns.args[1] });
-}
-export {
-  main
-};
--- home/weaken.js
+++
@@ -1,7 +0,0 @@
-// servers/home/weaken.js
-async function main(ns) {
-  await ns.weaken(ns.args[0]);
-}
-export {
-  main
-};
